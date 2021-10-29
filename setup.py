cc8ff57652941214b44c1b1fbf3330fb146df221
jurra <jose.urra86@gmail.com>
2021-10-29 17:00:13 +0200

    refactored important content chunks in the computing guides, and created the skeleton for research data guides. Moved hands on examples and tutorials into proper directories.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="example",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        'jupyter-book',
+    ]
+)
---
+++ start.sh
@@ -0,0 +1,11 @@
+# Deactivate the current environement
+# conda deactivate
+
+# Create environment for project
+python3 -m venv .env
+
+# Activate environment
+source .env/bin/activate
+
+# Install dependencies from requirements.txt
+# pip install -r requirements.txt
